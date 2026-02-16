aaac2b9741d92d55401499454b2ba90003e5dfaa
Stefan Dieterle <golgoths@yahoo.fr>
2026-02-16 16:45:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from recipes-cms!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/recipe_cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^-7*4ht%6a62iky6h+7zh-f2l8t)!@6add%$!e%v73geb9wmzb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
