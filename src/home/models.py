0b37ed42bcf80a61a881dac1acf5cf7d0204bf42
Stefan Dieterle <golgoths@yahoo.fr>
2026-02-16 17:21:37 +0100

    Add description field

--- src/home/models.py
+++ src/home/models.py
@@ -4,4 +4,7 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    description = models.CharField(max_length=255, blank=True, null=True)
+    api_fields = ["title", "description"]
+    parent_page_types = ["wagtailcore.Page"]
+    max_count = 1
