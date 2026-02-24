55e6a5609f1c3c62594596df921bacd2e76b91f0
Stefan Dieterle <golgoths@yahoo.fr>
2026-02-24 20:06:29 +0100

    Use postgres in production

--- src/recipe_cms/settings/dev.py
+++ src/recipe_cms/settings/dev.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import *  # noqa: F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -10,9 +10,3 @@ SECRET_KEY = "django-insecure-^-7*4ht%6a62iky6h+7zh-f2l8t)!@6add%$!e%v73geb9wmzb
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- src/recipe_cms/settings/production.py
+++ src/recipe_cms/settings/production.py
@@ -1,6 +1,24 @@
+import os
+
 from .base import *  # noqa: F403
 
 DEBUG = False
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# Database configuration for PostgreSQL
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "recipes_cms"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+    }
+}
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
